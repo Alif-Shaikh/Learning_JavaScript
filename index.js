5343ae679b5a86f68b3ceff5bd20dc4dc2bb9783
Alif <imalif.shaikh999@gmail.com>
2024-01-11 21:48:11 +0530

    javascript variables and data types

---
+++ index.js
@@ -0,0 +1,56 @@
+//1.ways to print in Javascript
+
+    //console.log("Hello World");
+    // alert("Hello Alif");
+    document.write("Hello JS World"); //not recommanded
+
+
+    //2.Javascript console apis
+
+        console.log("Hello  World");
+        console.log("Hello World",6+8,"another log");
+        console.warn("This is a warning!!!");
+        console.error("This is a error");
+        // console.clear();     to clear the window
+        console.assert(4==6);//assertion failed
+        console.assert(4==(5-1));//assertion passed
+
+
+  //3. Javascript variables
+ 
+  var number1=34;
+  var number2=56;
+  console.log(number1+number2);
+
+  //Data Types in Javascript
+   //Numbers
+  var num1=455;
+  var num2=56.76;
+
+  //String
+  var str1="This is a string";
+  var str2='This is a string';
+
+  //Objects
+  var marks={
+    Ravi:34,
+    Shubham:78,
+    Harry:99.977
+  }
+
+  console.log(marks);
+
+  //Booleans
+  var a=true;
+  var b=false;
+  console.log(a,b);
+
+  //Undefined
+  var und;  //dont know what to put
+ var und=undefined;
+ console.log(undefined);
+ console.log(und);
+
+//Null
+var n=null;
+console.log(n);
